eae3485783dcf6ef8fbe076df601b46973abdfb9
atanaskova <atanaskova.tm@yahoo.com>
2021-08-27 04:57:41 +0200

    Sorted the most popular recipes for the home page

--- client/src/components/Dashboard/MostPopularRecipes.jsx
+++ client/src/components/Dashboard/MostPopularRecipes.jsx
@@ -7,14 +7,25 @@ import Recipe from '../Recipe/Recipe';
 
 const MostPopularRecipes=()=>{
     const recipes=useSelector((state)=>state.recipes)
-    const last=recipes.slice(-3);
+    const popular=recipes;
+    
+    function mostPopular(a,b){
+        if(a.likes.length>b.likes.length){
+            return 1;
+        }else if(a.likes.length<b.likes.length){
+            return -1;
+        }
+        return 0;
+    }
+
+    popular.sort(mostPopular);
 
     return (
         !recipes.length ? 
         <SkeletonTheme color="#F5BE14"> 
-            <Skeleton className="skeleton" count={3}/>
+            <Skeleton className="skeleton" count={6}/>
         </SkeletonTheme> : (             
-        <div className="container-fluid" style={{width:780}}>
+        <div className="container-fluid" style={{width:800}}>
 
              <div className="separator">
                 <h2>Most Popular Recipes</h2>
@@ -22,7 +33,7 @@ const MostPopularRecipes=()=>{
             </div>
 
             <div className="row" style={{paddingTop:20}}>
-                {last.map((recipe)=>(
+                {popular.slice(-6).map((recipe)=>(
                     <div 
                         key={recipe._id}
                         className="card">
